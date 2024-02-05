759f1121e102cdb34e0a970bc14a20eafa2ec368
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-05 14:54:15 +0400

    add func to the sign-up

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,9 +1,9 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
-  const { username, email, password } = req.body;
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
   if (
     !username ||
@@ -13,21 +13,19 @@ export const signup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, 'All fields are required'));  }
-
-
-  const hashedPassword= bcryptjs.hashSync(password,10)
+    return next(errorHandler(400, "All fields are required"));
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
     email,
-    password:hashedPassword,
+    password: hashedPassword,
   });
 
   try {
-    
-      await newUser.save();
-      res.json("signup successful");
+    await newUser.save();
+    res.json("Signup successful");
   } catch (error) {
     next(error);
   }

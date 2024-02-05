73e09da52c481ffbf15ffcc0f18f3f439b67b3f3
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-05 10:49:44 +0400

    connect to the db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express'
-const app=express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mogose salan");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000,()=>{
-    console.log("zeynus");
-})
+const app = express();
+
+app.listen(3000, () => {
+  console.log("zeynus");
+});

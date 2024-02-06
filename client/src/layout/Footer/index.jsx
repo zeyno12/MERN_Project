fb74159581e369829acbf37ab6ec1d83a1cdaad1
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-06 15:28:52 +0400

    add reudx persist

--- client/src/layout/Footer/index.jsx
+++ client/src/layout/Footer/index.jsx
@@ -33,7 +33,7 @@ export default function FooterCom() {
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  Zeynab's Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>

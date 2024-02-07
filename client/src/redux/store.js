e02f7a5bb0c0a113aff9e5bab7f7e32d12101525
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-07 13:52:21 +0400

    add dark mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import themeReducer from "./theme/themeSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
 const rootReducer = combineReducers({
   user: userReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {

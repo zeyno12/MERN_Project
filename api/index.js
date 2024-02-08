3df76a3bc29e04303712f8bf547ab69783b72b3f
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-08 16:13:42 +0400

    update user api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.router.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.router.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -14,16 +16,17 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("zeynus");
 });
-app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,

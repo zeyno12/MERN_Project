b3bb9a3f8db52d980ff1786d2ac71f959fc5ba0e
zeyno12 <zeynabbagirova1@gmail.com>
2024-02-18 22:02:48 +0400

    edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,9 @@ import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
 import Search from "./pages/Search";
+
+
+
 export default function App() {
   return (
     <BrowserRouter>
